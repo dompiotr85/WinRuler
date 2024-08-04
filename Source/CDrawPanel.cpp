1b53ecc875850eaea5f3b4b035c425f6647ce3d3
Piotr Domański <11815021+dompiotr85@users.noreply.github.com>
2024-08-04 14:38:28 +0200

    Move comments from CDrawPanel.cpp file to CDrawPanel.h for cosmetic changes

--- Source/CDrawPanel.cpp
+++ Source/CDrawPanel.cpp
@@ -34,36 +34,18 @@ CDrawPanel::CDrawPanel(wxFrame* parent) :
     // No code.
 }
 
-/**
- * Called by the system of by wxWidgets when the panel needs to be redrawn.
- * You can also trigger this call by calling Refresh()/Update().
- **/
 void CDrawPanel::PaintEvent(wxPaintEvent& event)
 {
     wxPaintDC dc(this);
     Render(dc);
 }
 
-/**
- * Alternatively, you can use a clientDC to paint on the panel at any time.
- * Using this generally does not free you from catching paint events, since it
- * is possible that e.g. the window manager throws away your drawing when
- * the window comes to the background, and expects you will redraw it when
- * the window comes back (by sending a paint event).
- *
- * In most cases, this will not be needed at all; simply handling paint events
- * and calling Refresh() when a refresh is needed will do the job.
- **/
 void CDrawPanel::PaintNow()
 {
     wxClientDC dc(this);
     Render(dc);
 }
 
-/**
- * Here we do the actual rendering. I put it in a separate method so that it
- * can work no matter what type of DC (e.g. wxPaintDC or wxClientDC) is used.
- **/
 void CDrawPanel::Render(wxDC& dc)
 {
     // Draw some text.
--- Source/CDrawPanel.h
+++ Source/CDrawPanel.h
@@ -30,9 +30,28 @@ class CDrawPanel :
 public:
     CDrawPanel(wxFrame* parent);
 
+    /**
+     * Called by the system of by wxWidgets when the panel needs to be redrawn.
+     * You can also trigger this call by calling Refresh()/Update().
+     **/
     void PaintEvent(wxPaintEvent& event);
+	
+    /**
+     * Alternatively, you can use a clientDC to paint on the panel at any time.
+     * Using this generally does not free you from catching paint events, since it
+     * is possible that e.g. the window manager throws away your drawing when
+     * the window comes to the background, and expects you will redraw it when
+     * the window comes back (by sending a paint event).
+     * 
+     * In most cases, this will not be needed at all; simply handling paint events
+     * and calling Refresh() when a refresh is needed will do the job.
+     **/
     void PaintNow();
 
+    /**
+     * Here we do the actual rendering. I put it in a separate method so that it
+     * can work no matter what type of DC (e.g. wxPaintDC or wxClientDC) is used.
+     **/
     void Render(wxDC& dc);
 
     DECLARE_EVENT_TABLE()
